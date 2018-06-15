cc4c3f8ff9f1e2a4764639f46c91e33ec03b8387
Vedang Manerikar <ved.manerikar@gmail.com>
2018-06-15 23:49:31 +0530

    Add log4j2 for proper logging to a file.

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-fdb "0.1.0"]]
-  :main ^:skip-aot farstar.core
+                 [clj-fdb "0.1.0"]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [org.apache.logging.log4j/log4j-api "2.11.0"]
+                 [org.apache.logging.log4j/log4j-core "2.11.0"]]
   :codox {:source-uri "https://github.com/vedang/clj_fdb/blob/master/{filepath}#L{line}"
           :doc-files ["README.md"]
           :metadata {:doc/format :markdown}}
